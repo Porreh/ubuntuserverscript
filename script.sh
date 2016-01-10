cf73de1d26a5ce610fa3bc6a4bc334e40952cca2
Dmytro <Porreh@gmail.com>
2016-01-10 14:43:15 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,12 +2,10 @@
 # -*- Mode: sh; coding: utf-8 -*-
 
 sudo add-apt-repository -y ppa:elementary-os/stable
-sudo apt-add-repository -y ppa:ricotz/docky 
-sudo apt-add-repository -y ppa:marlin-devs/marlin-daily
-sudo apt-add-repository -y ppa:versable/elementary-update
+sudo apt-add-repository -y ppa:ricotz/docky
 
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get -y dist-upgrade
 
-sudo apt-get install -y pantheon-shell elementary-tweaks wingpanel plank lightdm-webkit-greeter pantheon-lightdm-theme pantheon-terminal pantheon-xsession-settings contractor slingshot-launcher scratch marlin elementary-theme elementary-icon-theme exe-wrapper ttf-droid plank-theme-pantheon switchboard-gnome-control-center preload
+sudo apt-get install -y pantheon-desktop
